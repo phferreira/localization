326de65ce27eb006280719f7a2cd5750de617dea
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2021-06-08 16:24:45 -0300

    Change initial module to splash screen

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_modular/flutter_modular.dart';
-
-import 'modules/home/home_module.dart';
+import 'package:localization_dart/app/modules/splash_screen/splashScreen_module.dart';
 
 class AppModule extends Module {
   @override
@@ -8,7 +7,6 @@ class AppModule extends Module {
 
   @override
   final List<ModularRoute> routes = [
-    ModuleRoute(Modular.initialRoute, module: HomeModule()),
+    ModuleRoute(Modular.initialRoute, module: SplashScreenModule()),
   ];
-
 }
