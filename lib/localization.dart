ac19da7141313e030b3ad65bac54b793ca4a3af1
phferreira <p.h.ferreirah@gmail.com>
2021-12-15 22:13:23 -0300

    Refactor: Convert to single quotes

--- lib/localization.dart
+++ lib/localization.dart
@@ -13,8 +13,8 @@ class Localization {
   static Map<String, String> _messages = Map();
   static Map<String, String> _labels = Map();
 
-  static const String _translationLocale = "assets/lang";
-  static const String _defaultLang = "en_US";
+  static const String _translationLocale = 'assets/lang';
+  static const String _defaultLang = 'en_US';
 
   static Future<String> decompressFile(String locale) async {
     final File file = File(locale);
@@ -32,7 +32,7 @@ class Localization {
     String translationLocale = _translationLocale,
     String translationLang = _defaultLang,
   }) async {
-    debugPrint("Loading localization data.");
+    debugPrint('Loading localization data.');
     String data;
 
     try {
@@ -45,18 +45,18 @@ class Localization {
 
     final Map<String, dynamic> _result = json.decode(data);
 
-    _result["labels"].forEach((String key, dynamic value) {
+    _result['labels'].forEach((String key, dynamic value) {
       _labels[key] = value.toString();
     });
-    _result["messages"].forEach((String key, dynamic value) {
+    _result['messages'].forEach((String key, dynamic value) {
       _messages[key] = value.toString();
     });
-    debugPrint("Localization data loaded successfully!");
+    debugPrint('Localization data loaded successfully!');
   }
 
   static String _i18n(String key, Map<String, String> map, [List<String> args = const []]) {
     if (map.isEmpty) {
-      throw "[Localization System] sentences is empty";
+      throw 'Localization sentences is empty';
     }
     String res = '';
     if (map.containsKey(key)) {
